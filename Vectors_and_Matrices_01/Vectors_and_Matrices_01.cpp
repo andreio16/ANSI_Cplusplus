7ba9f0033c6f3b25c97cd901ba94675433058fdf
Andrei Oana <andreioana97@yahoo.com>
2020-09-09 21:27:22 +0300

    Exercises added

--- Vectors_and_Matrices_01/Vectors_and_Matrices_01.cpp
+++ Vectors_and_Matrices_01/Vectors_and_Matrices_01.cpp
@@ -6,20 +6,91 @@
 
 int main()
 {
-	int a[100];
+	int a[100], c[10], d[10];
 	int b[10] = { 1, 2, 3, 4 };
-	float x[] = { 3.14, 2, 4.5 };
+	double x[] = { 3.14, 2, 4.5 };
+	char S[100];
+	char s[20] = "Hello world!";
+	char e[] = "ABC";
+	char f[] = { 'x','y','z' };
 	cout << "Vector a uses " << sizeof(a) << " bytes.\n";
 	cout << "Vector b uses " << sizeof(b) << " bytes.\n";
+	cout << "Vector S uses " << sizeof(S) << " bytes.\n";
+	cout << "Vector s uses " << sizeof(s) << " bytes.\n";
+	cout << "Vector e uses " << sizeof(e) << " bytes.\n";
+	cout << "Vector f uses " << sizeof(f) << " bytes.\n";
 	cout << "Vector x uses " << sizeof(x) << " bytes.\n";
-	cout << "Vector x has " << sizeof(x)/sizeof(int) << " values.\n";
+	cout << "Vector x has " << sizeof(x)/sizeof(double) << " values.\n";
+	cout << "Size of one single character ('A') is " << sizeof('A') << " bytes.\n";
+	cout << "Size of a string who contains one character (\"A\") is" << sizeof("A") << " bytes.\n";
 	newline();
-
 	input_vector(a);
 	newline();
 
-	cout << "Vector b : {";  print_vector(b, 4);  cout << "}\n";
-	cout << "Reverse vector b : {";  print_reverse_order_vector(b, 4);  cout << "}\n";
+	cout << "Vector b : { ";  print_vector(b, 4);  cout << "}\n";
+	cout << "Reverse vector b : { ";  print_reverse_order_vector(b, 4);  cout << "}\n";
+	cout << "Generate vector : { ";  generate_vector_v1(a, 10); print_vector(a, 10);  cout << "}\n";
+	cout << "Generate vector : { ";  generate_vector_v2(a, 10); print_vector(a, 10);  cout << "}\n";
+	cout << "Generate vector : { ";  generate_vector_v3(c, 10); print_vector(c, 10);  cout << "}\n"; 
+	cout << "Generate vector : { ";  generate_vector_v4(a, 10); print_vector(a, 10);  cout << "}\n";
+
+	cout << "Calculate sum = " << calculate_sum(a, 10) << "\n";
+	cout << "Calculate product of even numbers = " << calculate_mul_of_even_elements(a, 10) << "\n";
+	cout << "Calculate max value = " << max_value_from_vector(a, 10) << "\n";
+	check_asc_fibo_sim(c, 10);
+
+	newline();
+	cout << "Vector a : { ";  print_vector(a, 10);  cout << "}\n";
+	bubble_sort(a, 10);
+	cout << "Bubble sort applied : { ";  print_vector(a, 10);  cout << "}\n";
+
+	newline();
+	generate_vector_v2(c, 10);
+	cout << "Vector c : { ";  print_vector(c, 10);  cout << "}\n";
+	selection_sort(c, 10);
+	cout << "Selection sort applied : { ";  print_vector(c, 10);  cout << "}\n";
 	
+	newline();
+	generate_vector_v5(c, 10);
+	cout << "Vector c : { ";  print_vector(c, 10);  cout << "}\n";
+	generate_vector_v6(c, 10);
+	cout << "Vector c : { ";  print_vector(c, 10);  cout << "}\n";
+	generate_vector_v7(c, 10);
+	cout << "Vector c : { ";  print_vector(c, 10);  cout << "}\n";
+
+	print_string(s, strlen(s));
+	make_string_uppercase(s, strlen(s));
+	print_string(s, strlen(s));
+	make_string_lowercase(s, strlen(s));
+	print_string(s, strlen(s));
+	cout << "String contains " << count_words(s, strlen(s)) << " words.\n";
+	cout << "String contains " << count_words(e, strlen(e)) << " words.\n";
+	cout << "String contains " << count_words(f, strlen(f)) << " words.\n";
+	cout << "Enter your string : ";
+	cin.getline(S, 100);
+	cout << "Your string contains " << count_words(S, strlen(S)) << " words.\n";
+
+	newline();
+	int A[10][10];
+	int B[4][3] = { {1,2},{3,4},{5,6} };
+	cout << "Matrix A uses " << sizeof(A) << " bytes.\n";
+	cout << "Matrix B uses " << sizeof(B) << " bytes.\n";
+	
+	generate_matrix(A, 10);
+	print_matrix(A, 10);
+	newline();
+
+	int mat[3][3] = { {2,2,2},{2,2,2},{2,2,2} };
+	int v[3] = { 2,2,2 };
+	cout << "Your matrix is : \n"; 
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+			cout << mat[i][j] << " ";
+		cout << endl;
+	}
+	cout << "Your vector is "; print_vector(v, 3);
+	cout << "\nVector appears : " << count_vector_in_matrix(mat, v) << " times in the matrix.\n";
+
 }
 
--- Vectors_and_Matrices_01/functions_01.h
+++ Vectors_and_Matrices_01/functions_01.h
@@ -7,6 +7,9 @@
 //   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
 
 #include <iostream>
+#include <vector>
+#include <string.h>
+
 using namespace std;
 
 void newline()
@@ -37,10 +40,204 @@ void print_vector(int a[], int len)
 		cout << a[i] << " ";
 }
 
+void print_string(char s[], int len)
+{
+	for (int i = 0; i < len; i++)
+		cout << s[i] << " ";
+	cout << endl;
+}
+
 void print_reverse_order_vector(int a[], int len)
 {
 	for (int i = len - 1; i >= 0; i--)
 		cout << a[i] << " ";
+
 }
 
+void generate_vector_v1(int a[], int len)
+{
+	for (int i = 0; i < len; i++)
+		a[i] = len - 1 - i;
+}
 
+void generate_vector_v2(int a[], int len)
+{
+	for (int i = 0; i < len; i++)
+		a[i] = (i % 2 == 0) ? 0 : i;
+}
+
+void generate_vector_v3(int a[], int len)
+{
+	// Fibonacci Series
+	a[0] = a[1] = 1;
+	for (int i = 2; i < len; i++)
+		a[i] = a[i - 1] + a[i - 2];
+}
+
+void generate_vector_v4(int a[], int len)
+{
+	for (int i = 0; i < len; i++)
+		a[i] = -10 + rand() % (10 + 10 + 1);
+}
+
+void generate_vector_v5(int a[], int len)
+{
+	for (int i = 0; i < len; i++)
+		a[i] = rand() % rand();
+}
+
+void generate_vector_v6(int *a, int n)
+{
+	for (int i = 0; i < n; i++)
+		if (i % 2 == 0) a[i] = 0;
+		else a[i] = i / 2 + 1;
+}
+
+int calculate_sum(int a[], int len)
+{
+	int sum = 0;
+	for (int i = 0; i < len; i++)
+		sum += a[i];
+	return sum;
+}
+
+int calculate_mul_of_even_elements(int a[], int len)
+{
+	int mul = 1;
+	for (int i = 0; i < len; i++)
+		if (a[i] % 2 == 0) mul *= a[i];
+	return mul;
+}	
+
+int max_value_from_vector(int a[], int len)
+{
+	int max = (int)-INTMAX_MAX;
+	for (int i = 0; i < len; i++)
+		if (a[i] > max) max = a[i];
+	return max;
+}
+
+void check_asc_fibo_sim(int a[], int len)
+{
+	bool asc = true, fibo, sim;
+	for (int i = 0; i < len - 1; i++)
+		if (a[i] > a[i + 1])  asc = false;
+
+	if (a[0] == a[1] == 1) fibo = true;
+	for (int i = 2; i < len; i++)
+		if (a[i] != a[i - 1] + a[i - 2])fibo = false;
+	
+	for (int i = 0, j = len - 1; i < len; i++, j--)
+		if (a[i] != a[j]) sim = false;
+	
+	cout << "Given vector : is ";
+	if (!asc) cout << "not ";
+	cout << "ascending, ";
+	if (!fibo) cout << "not ";
+	cout << "Fibonacci, ";
+	if (!sim) cout << "not ";
+	cout << "symmetric. \n";
+
+	
+
+}
+
+void bubble_sort(int a[], int len)
+{
+	int aux;
+	for (int k = 0; k < len; k++)
+		for (int i = 0; i < len-1; i++)
+			if (a[i] > a[i + 1]) 
+			
+{
+				aux = a[i];
+				a[i] = a[i + 1];
+				a[i + 1] = aux;
+			}
+}
+
+void selection_sort(int *a, int len)
+{
+	for (int i = 0; i < len - 1; i++)
+		for (int j = i + 1; j < len; j++)
+			if (a[j] < a[i]) swap(a[j], a[i]);
+}
+
+void generate_matrix(int a[][10], int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if ((i == j) || (i + j == 10 - 1))
+				a[i][j] = 1;
+			else a[i][j] = 0;
+		}
+	}		
+}
+
+void print_matrix(int a[][10], int n)
+{
+	for (int i = 0; i < n; i++) 
+	{
+		for (int j = 0; j < n; j++)
+			cout << a[i][j] << " ";
+		cout << endl;
+	}
+}
+
+void generate_vector_v7(int *a, int n)
+{
+	int pos = 0, neg = n - 1, aux;
+
+	for (int i = 0; i < n; i++)
+	{
+		aux = -100 + rand() % (100 + 100 + 1);
+		if (aux > 0)
+			a[pos++] = aux;
+		else
+			a[neg--] = aux;		
+	}
+}
+
+int count_vector_in_matrix(int a[3][3], int *b)
+{
+	int ct = 0;
+	bool check_line;
+	bool check_col;
+	for (int i = 0; i < 3; i++)
+	{
+		check_line = true;
+		check_col = true;
+		for (int j = 0; j < 3; j++)
+		{
+			if (a[i][j] != b[j]) check_line = false;
+			if (a[j][i] != b[j]) check_col = false;
+		}
+		if (check_line == true) ct++;
+		if (check_col == true) ct++;
+	}
+	return ct;
+}
+
+void make_string_uppercase(char *s, int len)
+{
+	for (int i = 0; s[i]; i++)
+		if (s[i] >= 'a' && s[i] < 'z')
+			s[i] = s[i] - 'a' + 'A';
+}
+
+void make_string_lowercase(char *s, int len)
+{
+	for (int i = 0; s[i]; i++)
+		if (s[i] >= 'A' && s[i] <= 'Z')
+			s[i] = s[i] - 'A' + 'a';
+}
+
+int count_words(char *s, int len)
+{
+	int ct = 1;
+	for (int i = 0; s[i]; i++)
+		if (s[i] == ' ') ct++;
+	return ct;
+}
